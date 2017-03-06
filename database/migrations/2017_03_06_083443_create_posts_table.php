604edf86551d150e75418edd915d6c695996afa6
student <student@Win64-60.ue.loc>
2017-03-06 10:39:48 +0100

    06032017 - comit (tables migration added)

--- database/migrations/2017_03_06_083443_create_posts_table.php
+++ database/migrations/2017_03_06_083443_create_posts_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('content');
+            $table->integer('likes_counter');
             $table->timestamps();
         });
     }
@@ -29,6 +28,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
-    }
+        if(Schema::hasTable('posts')){
+        Schema::drop('posts');
+        
+    }}
 }
